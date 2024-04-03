15f9a82b5629b450d06dc2cf2625e7b2d74035ae
아이디email <ssjj0229@gmail.com>
2024-04-03 13:06:32 +0900

    어렵다

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
-
+import { useState } from "react";
+import TodoTemplate from "./components/TodoTemplate";
+import { MdAdd } from "react-icons/md";
+import TodoInsert from "./components/Todoinsert";
+import TodoListItem from "./components/TodoListItem";
+import TodoList from "./components/TodoList";
 function App() {
   return (
-    <div>
-      <h1>할 일 앱 만들기</h1>
-    </div>
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
   );
 }
 
